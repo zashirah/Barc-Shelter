d87a615643a6cbcc4ba1ae19f2a6b4f69d44bd97
VonArzu <Von.Arzu@rutgers.edu>
2020-08-21 16:39:49 -0400

    Nav

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -6,15 +6,30 @@ const Nav = () => {
   return (
     <nav>
       <div className="nav">
-        <NavLink className="logo" to="/">
-          ProductsApp
-        </NavLink>
         <div className="links">
-          <NavLink className="link" to="/products">
-            Products
+          <NavLink className="link" to="/Home">
+            Home
           </NavLink>
-          <NavLink className="link" to="/add-product">
-            Add Product
+          <NavLink className="link" to="/">
+            Adopt
+          </NavLink>
+          <NavLink className="link" to="/">
+            Volunteer
+          </NavLink>
+          <NavLink className="link" to="/">
+            Donate
+          </NavLink>
+          <NavLink className="link" to="/">
+            Events
+          </NavLink>
+          <NavLink className="link" to="/">
+            Contact
+          </NavLink>
+          <NavLink className="link" to="/">
+            About
+          </NavLink>
+          <NavLink className="link" to="/">
+            More...
           </NavLink>
         </div>
       </div>
