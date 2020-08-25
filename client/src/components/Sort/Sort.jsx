27c1c64fb1b2b0da3a28919785ba838c8157e7a9
jvsnyder <forexsnyder@gmail.com>
2020-08-25 09:14:46 -0700

    sort button

--- client/src/components/Sort/Sort.jsx
+++ client/src/components/Sort/Sort.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import './Sort.css'
 
+
 const Sort = (props) => {
 
     const handleChange = (event) => {
         props.onChange(event.target.value)
     }
 
-    return (
-        <form className="sort-container" onSubmit={props.handleSubmit}>
+  return (
+    <div className="navbar">
+        <div className="dropdown">
+        <button className="dropbtn">Sort By   <i className="fa fa-caret-down" aria-hidden="true" onClick={props.handleSubmit}></i></button>
+          <div className="dropdown-content" onClick={handleChange}>
+                <option className="option" value="age-ascending">&nbsp; Age, (Young to Old) &nbsp;</option>
+                <option className="option" value="age-descending">&nbsp; Age, (Old to Young) &nbsp;</option>
+                <option className="option" value="shelteredTime-ascending">&nbsp; Sheltered Time, Short to Long &nbsp;</option>
+                <option className="option" value="shelteredTime-descending">&nbsp; Sheltered Time, Long to Short &nbsp;</option>
+          </div>
+            {/* <form className="sort-container" onSubmit={props.handleSubmit}>
             <label htmlFor="sort">SORT BY:</label>
             <select className="sort" onChange={handleChange}>
                 <option className="option" value="age-ascending" >&nbsp; Age, (Young to Old) &nbsp;</option>
@@ -16,8 +26,15 @@ const Sort = (props) => {
                 <option value="shelteredTime-ascending">&nbsp; Sheltered Time, Short to Long &nbsp;</option>
                 <option value="shelteredTime-descending">&nbsp; Sheltered Time, Long to Short &nbsp;</option>
             </select>
-        </form>
-    )
+        </form> */}
+    </div>
+
+
+
+    </div>
+
+
+  )
 }
 
 export default Sort
