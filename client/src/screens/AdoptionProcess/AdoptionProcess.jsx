43ff3935f7f20178293477177c96d9fc4c288cea
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-08-24 22:19:50 -0400

    updated MainButton to allow color changes to be passed through props; then added the button to the application process page

--- client/src/screens/AdoptionProcess/AdoptionProcess.jsx
+++ client/src/screens/AdoptionProcess/AdoptionProcess.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 import SecondaryHeaderImage from '../../components/SecondaryHeaderImage/SecondaryHeaderImage'
 import AdoptionProcessNodes from "../../components/adoptionProcess/AdoptionProcessNodes/AdoptionProcessNodes";
 import AdoptionProcessStart from "../../components/adoptionProcess/AdoptionProcessStart/AdoptionProcessStart";
+import MainButton from '../../components/MainButton/MainButton'
 
 import headerImage from './adoptionProcessImage.png'
+
+import "./AdoptionProcess.css"
 
 const AdoptionProcess = () => {
   return (
@@ -12,6 +15,10 @@ const AdoptionProcess = () => {
       <SecondaryHeaderImage image={headerImage} />
       <AdoptionProcessNodes />
       <AdoptionProcessStart />
+      <div className="adoption-process-buttons">
+        <MainButton buttonText={"View & Print an Offline Application"} buttonColor={"gray"} />
+        <MainButton buttonText={"Start Application Online"} buttonColor={"orange"} />
+      </div>
     </div>
   );
 }
