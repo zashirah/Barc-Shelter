d7862e2445610a6aa5878acf36ea37437a8717be
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-08-24 21:04:27 -0400

    added previous button and styled it

--- client/src/components/adoptionProcess/AdoptionProcessNodes/AdoptionProcessNodes.jsx
+++ client/src/components/adoptionProcess/AdoptionProcessNodes/AdoptionProcessNodes.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import NodeCircle from './NodeCircle/NodeCircle'
 import NodeLine from "./NodeLine/NodeLine";
+import PreviousButton from "../PreviousButton/PreviousButton"
 
 import "./AdoptionProcessNodes.css"
 
 const AdoptionProcessNodes = () => {
   return (
     <div className="adoption-process-nodes">
+      <PreviousButton />
       <NodeCircle number={1} nodeCircleStage={"previous-node"} />
       <NodeLine nodeLineStage={"previous-node-line"} />
       <NodeCircle number={2} nodeCircleStage={"current-node"} />
