08f7f2a8c70e39f1731e34348512d9a28b2dfa2e
Vukan Mrvaljevic <vukanmrvaljevic@vukans-mbp.home>
2020-08-25 17:29:29 -0400

    volunteer

--- client/src/screens/Donation/Donate.jsx
+++ client/src/screens/Donation/Donate.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import DonatePage from "../../components/DonatePage/DonatePage"
+// import NavBar from "../../components/shared/Nav/Nav"
 
 const Donate = () => {
   return (
     <div>
+      {/* <NavBar /> */}
       <DonatePage />
     </div>
   )
