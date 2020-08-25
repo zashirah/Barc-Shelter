2cad69a3f60cb7d6cc55a71649ab454c6b35b768
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-08-24 22:03:38 -0400

    created adoption process info section

--- client/src/components/adoptionProcess/AdoptionProcessStart/AdoptionProcessStart.jsx
+++ client/src/components/adoptionProcess/AdoptionProcessStart/AdoptionProcessStart.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 import APStartCTASection from "./APStartCTASection/APStartCTASection"
+import AdoptionProcessInfoSection from "./AdoptionProcessInfoSection/AdoptionProcessInfoSection"
 
 const AdoptionProcessStart = () => {
   return (
     <div>
       <APStartCTASection />
+      <AdoptionProcessInfoSection />
     </div>
-  )
+  );
 }
 
 export default AdoptionProcessStart
