e529d18d7bdd49bceab0bf190bfdb6bb4c28f9c8
VonArzu <Von.Arzu@rutgers.edu>
2020-08-25 17:22:10 -0400

    Layout on all screens

--- client/src/screens/AllPets/AllPets.jsx
+++ client/src/screens/AllPets/AllPets.jsx
@@ -1,31 +1,35 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import AnimalCard from '../../components/AnimalCard/AnimcalCard'
+import AnimalCard from "../../components/AnimalCard/AnimcalCard";
 
-import { getPets } from '../../services/pets'
+import { getPets } from "../../services/pets";
+
+import "../../components/shared/Layout/Layout";
+import Layout from "../../components/shared/Layout/Layout";
 
 const AllPets = () => {
-  const [allPets, setAllPets] = useState([])
-  const [queriedPets, setQueriedPets] = useState([])
+  const [allPets, setAllPets] = useState([]);
+  const [queriedPets, setQueriedPets] = useState([]);
 
   useEffect(() => {
     const fetchPets = async () => {
-      const pets = await getPets()
-      setAllPets(pets)
-      setQueriedPets(pets)
-    }
-    fetchPets()
-  }, [])
+      const pets = await getPets();
+      setAllPets(pets);
+      setQueriedPets(pets);
+    };
+    fetchPets();
+  }, []);
 
-  const petCardsJSX = queriedPets.map((pet, index) => 
-    <AnimalCard name={pet.name} key={index}/>
-  )
+  const petCardsJSX = queriedPets.map((pet, index) => (
+    <AnimalCard name={pet.name} key={index} />
+  ));
 
   return (
-    <div>
-      {petCardsJSX}
-    </div>
-  )
-}
+    <Layout>
+      {" "}
+      <div>{petCardsJSX}</div>
+    </Layout>
+  );
+};
 
-export default AllPets
+export default AllPets;
--- client/src/screens/PetDetail/PetDetail.jsx
+++ client/src/screens/PetDetail/PetDetail.jsx
@@ -1,38 +1,34 @@
-import React, { useState, useEffect } from 'react'
-import { getPet, deletePet } from '../../services/pets'
-import { useParams } from 'react-router-dom'
-
+import React, { useState, useEffect } from "react";
+import { getPet, deletePet } from "../../services/pets";
+import { useParams } from "react-router-dom";
+import Layout from "../../components/shared/Layout/Layout";
 
 const PetDetail = () => {
-  const [pet, setPet] = useState(null)
-  const { id } = useParams()
+  const [pet, setPet] = useState(null);
+  const { id } = useParams();
 
   useEffect(() => {
     const fetchPet = async () => {
-      const pet = await getPet(id)
-      console.log(pet)
-      setPet(pet)
-    }
-    fetchPet()
-  }, [id])
+      const pet = await getPet(id);
+      console.log(pet);
+      setPet(pet);
+    };
+    fetchPet();
+  }, [id]);
 
   if (pet) {
-  return (
-    <div>
-      <h1>
-        {pet.name}
-      </h1>
-      <button>Edit</button>
-      <button onClick={() => deletePet(pet._id)}>Delete</button>
-    </div>
-  )
-  } else {
     return (
-      <div>
-        loading
-      </div>
-    )
+      <Layout>
+        <div>
+          <h1>{pet.name}</h1>
+          <button>Edit</button>
+          <button onClick={() => deletePet(pet._id)}>Delete</button>
+        </div>
+      </Layout>
+    );
+  } else {
+    return <div>loading</div>;
   }
-}
+};
 
-export default PetDetail
+export default PetDetail;
