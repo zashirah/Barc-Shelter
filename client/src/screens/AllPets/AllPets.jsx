b0b7b24bfeffc9c2311fe8ec8da681735718d10f
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-08-26 12:25:52 -0400

    updated CSS

--- client/src/screens/AllPets/AllPets.jsx
+++ client/src/screens/AllPets/AllPets.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useLayoutEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./AllPets.css";
 
