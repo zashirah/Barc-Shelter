8938494db9be61de06f75554c6d252d46ce9265f
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-08-26 08:34:55 -0400

    nodes change colors based on stage

--- client/src/components/adoptionProcess/AdoptionProcessNodes/AdoptionProcessNodes.jsx
+++ client/src/components/adoptionProcess/AdoptionProcessNodes/AdoptionProcessNodes.jsx
@@ -10,13 +10,43 @@ const AdoptionProcessNodes = ({ updateStage, stage }) => {
   return (
     <div className="adoption-process-nodes">
       {stage !== 1 && <PreviousButton updateStage={updateStage} />}
-      <NodeCircle number={1} nodeCircleStage={"previous-node"} />
-      <NodeLine nodeLineStage={"previous-node-line"} />
-      <NodeCircle number={2} nodeCircleStage={"current-node"} />
-      <NodeLine nodeLineStage={"upcoming-node-line"} />
-      <NodeCircle number={3} nodeCircleStage={"upcoming-node"} />
-      <NodeLine nodeLineStage={"upcoming-node-line"} />
-      <NodeCircle number={4} nodeCircleStage={"upcoming-node"} />
+      <NodeCircle
+        number={1}
+        nodeCircleStage={stage === 1 ? "current-node" : "previous-node"}
+      />
+      <NodeLine
+        nodeLineStage={stage > 1 ? "previous-node-line" : "upcoming-node-line"}
+      />
+      <NodeCircle
+        number={2}
+        nodeCircleStage={
+          stage === 1
+            ? "upcoming-node"
+            : stage === 2
+            ? "current-node"
+            : "previous-node"
+        }
+      />
+      <NodeLine
+        nodeLineStage={stage > 2 ? "previous-node-line" : "upcoming-node-line"}
+      />
+      <NodeCircle
+        number={3}
+        nodeCircleStage={
+          stage === 4
+            ? "previous-node"
+            : stage === 3
+            ? "current-node"
+            : "upcoming-node"
+        }
+      />
+      <NodeLine
+        nodeLineStage={stage > 3 ? "previous-node-line" : "upcoming-node-line"}
+      />
+      <NodeCircle
+        number={4}
+        nodeCircleStage={stage === 4 ? "current-node" : "upcoming-node"}
+      />
     </div>
   );
 };
