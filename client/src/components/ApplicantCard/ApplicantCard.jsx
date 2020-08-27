96531c998588f74438561d6a9b949c6341a73914
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-08-27 12:25:59 -0400

    delete crud works

--- client/src/components/ApplicantCard/ApplicantCard.jsx
+++ client/src/components/ApplicantCard/ApplicantCard.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import "./ApplicantCard.css"
+import "./ApplicantCard.css";
 
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from "react-router-dom";
+import { deleteApplicant } from "../../services/applicant";
 
 const ApplicantCard = ({ applicant }) => {
+  const [isDeleted, setDeleted] = useState(false);
+
+  const handleDelete = () => {
+    deleteApplicant(applicant._id);
+    setDeleted(true)
+  };
+
+  if (isDeleted) {
+    return <Redirect to={`/applicants`} />;
+  }
+
   return (
     // <Link to={`/applicants/${applicant._id}`}>
-      <div className="applicant-card">
-        <div className="applicant-name-div">
-          Name: {applicant.applicantName}{" "}
-        </div>
-        <div className="applicant-appt-div">
-          Appointment Time: {applicant.appointment}
-        </div>
-        <Link to={`/applicants/${applicant._id}/edit`}>
-          <button>Edit</button>
-        </Link>
-        <button>Delete</button>
+    <div className="applicant-card">
+      <div className="applicant-name-div">Name: {applicant.applicantName} </div>
+      <div className="applicant-appt-div">
+        Appointment Time: {applicant.appointment}
       </div>
+      <Link to={`/applicants/${applicant._id}/edit`}>
+        <button>Edit</button>
+      </Link>
+      <button onClick={handleDelete}>Delete</button>
+    </div>
     // </Link>
   );
-}
+};
 
-export default ApplicantCard
+export default ApplicantCard;
