f7e70d4aa0087a7d9516fe1cd690f296768f02e3
jvsnyder <forexsnyder@gmail.com>
2020-08-26 17:17:53 -0700

    fixing sort button css

--- client/src/components/Sort/Sort.jsx
+++ client/src/components/Sort/Sort.jsx
@@ -9,24 +9,15 @@ const Sort = (props) => {
     }
 
   return (
-    <div className="navbar">
+    <div className="sort-button">
         <div className="dropdown">
-        <button className="dropbtn">Sort By   <i className="fa fa-caret-down" aria-hidden="true" onClick={props.handleSubmit}></i></button>
+        <button className="dropbtn">Sort   <i className="fa fa-caret-down" aria-hidden="true" onClick={props.handleSubmit}></i></button>
           <div className="dropdown-content" onClick={handleChange}>
-                <option className="option" value="age-ascending">&nbsp; Age, (Young to Old) &nbsp;</option>
-                <option className="option" value="age-descending">&nbsp; Age, (Old to Young) &nbsp;</option>
+                <option className="optin" value="age-ascending">&nbsp; Age, (Young to Old) &nbsp;</option>
+                <option className="optin" value="age-descending">&nbsp; Age, (Old to Young) &nbsp;</option>
                 {/* <option className="option" value="shelteredTime-ascending">&nbsp; Sheltered Time, Short to Long &nbsp;</option>
                 <option className="option" value="shelteredTime-descending">&nbsp; Sheltered Time, Long to Short &nbsp;</option> */}
           </div>
-            {/* <form className="sort-container" onSubmit={props.handleSubmit}>
-            <label htmlFor="sort">SORT BY:</label>
-            <select className="sort" onChange={handleChange}>
-                <option className="option" value="age-ascending" >&nbsp; Age, (Young to Old) &nbsp;</option>
-                <option value="age-descending">&nbsp; Age, (Old to Young) &nbsp;</option>
-                <option value="shelteredTime-ascending">&nbsp; Sheltered Time, Short to Long &nbsp;</option>
-                <option value="shelteredTime-descending">&nbsp; Sheltered Time, Long to Short &nbsp;</option>
-            </select>
-        </form> */}
     </div>
 
 
