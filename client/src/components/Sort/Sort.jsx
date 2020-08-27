4d56c456ac3a37a1a57b076265ccd57b16d36eae
jvsnyder <forexsnyder@gmail.com>
2020-08-26 18:29:06 -0700

    sort button better coloring and shadowing

--- client/src/components/Sort/Sort.jsx
+++ client/src/components/Sort/Sort.jsx
@@ -9,19 +9,16 @@ const Sort = (props) => {
     }
 
   return (
-    <div className="sort-button">
-        <div className="dropdown">
-        <button className="dropbtn">Sort   <i className="fa fa-caret-down" aria-hidden="true" onClick={props.handleSubmit}></i></button>
-          <div className="dropdown-content" onClick={handleChange}>
+    <div>
+        <div className="dropdowna">
+        <button className="dropbtna">Sort   <i className="fa fa-caret-down" aria-hidden="true" onClick={props.handleSubmit}></i></button>
+          <div className="dropdown-contenta" onClick={handleChange}>
                 <option className="optin" value="age-ascending">&nbsp; Age, (Young to Old) &nbsp;</option>
                 <option className="optin" value="age-descending">&nbsp; Age, (Old to Young) &nbsp;</option>
                 {/* <option className="option" value="shelteredTime-ascending">&nbsp; Sheltered Time, Short to Long &nbsp;</option>
                 <option className="option" value="shelteredTime-descending">&nbsp; Sheltered Time, Long to Short &nbsp;</option> */}
           </div>
     </div>
-
-
-
     </div>
 
 
