f44c986c1192750c6ef25951a93671d89aa7026e
Vukan Mrvaljevic <vukanmrvaljevic@vukans-mbp.home>
2020-08-27 15:53:01 -0400

    fixed the home page css

--- client/src/components/HomePageCards/HomePageCards.jsx
+++ client/src/components/HomePageCards/HomePageCards.jsx
@@ -7,35 +7,37 @@ import aboutUs from "./about-us.jpg"
 
 const HomePageCards = () => {
   return (
-    <div className="cards-div">
-      <div>
-      <h1 className="happy-trails">Happy Trails</h1>
-      <div className="cat-in-cage">
-        <img src={catCage} alt="cat" />
+    <>
+      <div className="top-cards-div">
+        <div>
+          <h1 className="happy-trails">Happy Trails</h1>
+          <div className="cat-in-cage">
+            <img src={catCage} alt="cat" className="cat-cage" />
+          </div>
+        </div>
+        <div>
+          <h1 className="lost">Lost Dog/Cats</h1>
+          <div className="lost-dog-container">
+            <img src={lostDog} alt="lost dog" className="lost-dogs" />
+          </div>
+        </div>
       </div>
+      <div className="bottom-cards-div">
+        <div>
+          <h1 className="news-paper">Events</h1>
+          <div className="daily-kibble">
+            <img src={dailyKibble} alt="news" className="news" />
+          </div>
+        </div>
+        <div>
+          <h1 className="about">About Us</h1>
+          <div className="about-us">
+            <img src={aboutUs} alt="bio" className="about-company" />
+          </div>
+        </div>
       </div>
-      <div>
-      <h1 className="lost">Lost Dog/Cats</h1>
-      <div className="lost-dog">
-        <img src={lostDog} alt="lost" />
-      </div>
-      </div>
-      <div>
-      <h1 className="news-paper">Events</h1>
-      <div className="daily-kibble">
-        <img src={dailyKibble} alt="news" />
-      </div>
-      </div>
-      <div>
-      <h1 className="about">About Us</h1>
-      <div className="about-us">
-        <img src={aboutUs} alt="bio" />
-      </div>
-      </div>
-    </div>
+    </>
   )
 }
-
-
 
 export default HomePageCards
